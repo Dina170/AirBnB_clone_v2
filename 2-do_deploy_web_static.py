9204cdd232e4d459ca307db5924aa6e1cf10e14d
dina <dinaelhadidy966@gmail.com>
2023-09-07 16:36:02 -0400

    Add do_deploy function

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os import path
+
+env.hosts = ['52.3.249.4', '54.160.86.137']
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if not path.exists(archive_path):
+        return False
+    try:
+        full_file = archive_path.split("/")[-1]
+        file_name = full_file.split(".")[0]
+        path = "/data/web_static/releases/"
+        full_path = '{}{}'.format(path, file_name)
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}/'.format(full_path))
+        run('tar -xzf /tmp/{} -C {}/'.format(full_file, full_path))
+        run('rm /tmp/{}'.format(full_file))
+        run('mv {}/web_static/* {}/'.format(full_path, full_path))
+        run('rm -rf {}/web_static'.format(full_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(full_path))
+        return True
+    except:
+        return False
