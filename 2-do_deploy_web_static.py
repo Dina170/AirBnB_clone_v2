3b833c262adebb5c9801a891fc4d9981297326bd
dina <dinaelhadidy966@gmail.com>
2023-09-07 17:04:29 -0400

    debug

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,12 +13,16 @@ env.hosts = ['52.3.249.4', '54.160.86.137']
 def do_deploy(archive_path):
     """Distributes an archive to web servers"""
     if not path.exists(archive_path):
+    	print("arch not exist")
         return False
     try:
         full_file = archive_path.split("/")[-1]
+        print("full_file", full_file)
         file_name = full_file.split(".")[0]
+        print("file_name", file_name)
         dir_path = "/data/web_static/releases/"
         full_path = '{}{}'.format(dir_path, file_name)
+        print("fullpath", full_path)
         put(archive_path, '/tmp/')
         run('mkdir -p {}/'.format(full_path))
         run('tar -xzf /tmp/{} -C {}/'.format(full_file, full_path))
@@ -29,4 +33,5 @@ def do_deploy(archive_path):
         run('ln -s {}/ /data/web_static/current'.format(full_path))
         return True
     except:
+	print("exception")
         return False
