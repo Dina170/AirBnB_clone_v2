8b71f43627696914d3b1676fa3b4d5396e7bea9a
dina <dinaelhadidy966@gmail.com>
2023-09-24 16:56:53 -0400

    Route 2 paths to the same page

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,5 +25,14 @@ def c(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    """Display “Python ”, followed by the value of the text variable
+       The default value of text is “is cool”
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
