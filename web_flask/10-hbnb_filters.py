3e5ba38b98f5e97939170fbd81d98868c22ad138
dina <dinaelhadidy966@gmail.com>
2023-09-25 12:30:48 -0400

    Display data from DB in Airbnb clone

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Display HTML page state & city from DBStorage"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return (render_template('10-hbnb_filters.html', states=states,
+                            amenities=amenities))
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    """After each request remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
